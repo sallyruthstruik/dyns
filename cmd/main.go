04e4d45ffb9fafdb85999441cd489a2b83d585d1
Stanislav Kaledin <skaledin@ivi.ru>
2018-11-12 00:20:37 +0300

    Simple proxy to google DNS server

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,26 @@ package main
 import (
 	"net"
 	"log"
-	"encoding/hex"
 )
+
+func askGoogle(request []byte) (response []byte, err error){
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Write(request)
+	if err != nil {
+		return
+	}
+
+	buffer := make([]byte, 1024)
+	count, err := conn.Read(buffer)
+	if err != nil {
+		return
+	}
+	return buffer[:count], nil
+}
 
 func main() {
 	listener, err := net.ListenPacket("udp", "0.0.0.0:1054")
@@ -17,12 +35,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	buffer := make([]byte, 1)
-	count, _, err := listener.ReadFrom(buffer)
+	buffer := make([]byte, 1024)
+	count, addr, err := listener.ReadFrom(buffer)
 	if err != nil{
 		log.Fatal(err)
 	}
+	buffer = buffer[:count]
 
-	log.Println(count)
-	log.Println(hex.Dump(buffer))
+	g, err := askGoogle(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = listener.WriteTo(g, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
